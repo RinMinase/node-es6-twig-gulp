d4419dd0b2650442a9db7da410e4242e53390ef8
Rin Minase <rin.black.raison.detre@gmail.com>
2020-09-07 15:46:23 +0800

    Removed comments on code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-// import page from '../views/index.twig';
-// const page = require('../views/layout.twig');
-
 router.get('/', function(req, res, next) {
   res.render('index.twig', { title: 'Express' });
 });
-
-// router.get('/', (_request, response) => {
-// 	response.end('Express App');
-// });
 
 export default router;
