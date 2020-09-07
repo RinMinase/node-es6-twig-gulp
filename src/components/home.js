bb1ccb85b9cca69673c3145090bad35573df1484
Rin Minase <rin.black.raison.detre@gmail.com>
2020-09-07 18:15:00 +0800

    Added modularized styles

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('home.twig', { title: 'Express' });
+  res.render('home.twig', { title: 'Site' });
 });
 
 export default router;
